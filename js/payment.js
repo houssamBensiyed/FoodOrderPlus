b6e81e4d7187ea96772e622f848c5b77fa698e63
abderrahmane140 <bsarabderrahmane01@gmail.com>
2025-11-13 15:01:03 +0100

    rander the data form the localStorage

--- js/payment.js
+++ js/payment.js
@@ -84,3 +84,49 @@ const validateEmail = (email) =>{
 }
 
 submitBtn.addEventListener('click', validate)
+
+
+// let testingDqata = [
+//       {
+//     "id": 1,
+//     "image": "./images/salade-cesar.png",
+//     "name": "Salade César",
+//     "category": "entree",
+//     "description": "Salade croquante avec poulet grillé, parmesan et sauce césar maison.",
+//     "price": 6.5,
+//     "size": ["S", "M", "L", "XL"],
+//     "availability": true
+//   },
+//   {
+//     "id": 2,
+//     "image": "./images/soup-oignon.png",
+//     "name": "Soupe à l’oignon",
+//     "category": "entree",
+//     "description": "Soupe traditionnelle française avec croûtons et fromage fondu.",
+//     "price": 5.0,
+//     "size": ["S", "M", "L", "XL"],
+//     "availability": true
+//   },
+// ]
+
+
+// localStorage.setItem('datPpanier',JSON.stringify(testingDqata))
+const data = JSON.parse(localStorage.getItem('datPpanier'))
+
+
+if (data) {
+    const container = document.getElementById('products-payment');
+    
+    data.forEach(e => {
+        const div = `
+            <div class="bg-[#2F2828] h-28 p-5 rounded-lg text-white flex gap-8 items-center">
+                <div class="w-20 h-20 bg-white rounded-lg bg-[url(${e.image})] bg-cover bg-center"></div>
+                <div>
+                    <h1 class="text-xl">${e.name}</h1>
+                    <p>${e.price} MAD</p>
+                </div>
+            </div>`;
+        
+        container.innerHTML += div;
+    });
+}
